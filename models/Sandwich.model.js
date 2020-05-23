7abbe7458b519dd9a1fc6f1e8b477506beb44abb
Stuart Fong <stuartfong@Admins-MacBook-Pro.local>
2020-05-23 08:17:24 -0400

    fixed date and time module issue

--- models/Sandwich.model.js
+++ models/Sandwich.model.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose")
-const date = require('date-and-time');
 
 const orderSchema = mongoose.Schema({
     name: {
@@ -13,10 +12,6 @@ const orderSchema = mongoose.Schema({
     address: {
         type: String,
         required: true
-    },
-    date: {
-        type: Date,
-        default: calcTime("-5.0")
     },
     sandwichType: {
         type: String,
@@ -42,10 +37,6 @@ const orderSchema = mongoose.Schema({
         type:Boolean,
         default: false
     },
-    formattedDate: {
-        type: String, 
-        default: date.format(calcTime("-5.0"), 'hh:mm A')
-    },
     imageProfileNumber: {
         type: Number,
         required: true
@@ -56,12 +47,5 @@ const orderSchema = mongoose.Schema({
     }
 })
  
-//Stolen from 'Sudhir Bastakoti', https://stackoverflow.com/questions/8207655/get-time-of-specific-timezone
-function calcTime(offset) {
-    var d = new Date();
-    var utc = d.getTime() + (d.getTimezoneOffset() * 60000);
-    var nd = new Date(utc + (3600000*offset));
-    return nd
-}
 
 module.exports = mongoose.model("Sandwich", orderSchema)
