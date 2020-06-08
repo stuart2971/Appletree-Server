29b477b328d4cfb76b422274936a1725e330cfd7
Stuart Fong <stuartfong@Admins-MacBook-Pro.local>
2020-06-08 13:44:31 -0400

    testing still

--- server.js
+++ server.js
@@ -26,6 +26,20 @@ app.use((req, res, next) => {
     next();
 });
 
+const calculateOrderAmount = items => {
+    return 1400;
+};
+app.post("/create-payment-intent", async (req, res) => {
+    const { items } = req.body;
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: calculateOrderAmount(items),
+        currency: "usd"
+    });
+    res.send({
+        clientSecret: paymentIntent.client_secret
+    });
+});
+
 mongoose.connect(process.env.MONGO_CONNECTION, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology:true})
 const db = mongoose.connection;
 
